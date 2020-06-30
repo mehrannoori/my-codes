fd1ad077ff9919d707ed0636bd72cace98677332
mehran <52105888+mehrannoori@users.noreply.github.com>
2020-06-30 14:54:35 +0430

    Create mdf.cpp

---
+++ mdf.cpp
@@ -0,0 +1,27 @@
+/********************************************************
+print middle finger
+it is Bullshit, i know
+*********************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "                     /\"\\" << endl;
+	cout << "                    |\\./|" << endl;
+	cout << "                    |   |" << endl;
+	cout << "                    |   |" << endl;
+	cout << "                    |>~<|" << endl;
+	cout << "                    |   |" << endl;
+	cout << "                 /'\\|   |/'\\.." << endl;
+	cout << "             /~\\|   |   |   | \\" << endl;
+	cout << "            |   =[@]=   |   |  \\" << endl;
+	cout << "            |   |   |   |   |   \\" << endl;
+	cout << "            | ~   ~   ~   ~ |`   )" << endl;
+	cout << "            |                   /" << endl;
+	cout << "             \\                 /" << endl;
+	cout << "              \\               /" << endl;
+	cout << "               \\    _____    /" << endl;
+	cout << "                |--//''`\\--|" << endl;
+	cout << "                | (( +==)) |" << endl;
+	cout << "                |--\\_|_//--|" << endl;
+	return 0;
